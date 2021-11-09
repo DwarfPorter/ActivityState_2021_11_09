ee700f29ca6e50cb8e56ef10dd9288587c7cc377
DwarfPorter <mrvlmor@gmail.com>
2021-11-09 21:56:01 +0300

    кнопка 3 и 4

--- app/src/main/java/ru/geekbrains/activitystate/Counters.java
+++ app/src/main/java/ru/geekbrains/activitystate/Counters.java
@@ -3,6 +3,8 @@ package ru.geekbrains.activitystate;
 public class Counters {
     private int counter1 = 0;
     private int counter2 = 0;
+    private int counter3 = 0;
+    private int counter4 = 0;
 
     public int getCounter1() {
         return counter1;
@@ -12,11 +14,27 @@ public class Counters {
         return counter2;
     }
 
+    public int getCounter3() {
+        return counter3;
+    }
+
+    public int getCounter4() {
+        return counter4;
+    }
+
     public void incrementCounter1(){
         counter1++;
     }
 
     public void incrementCounter2(){
         counter2++;
+    }
+
+    public void incrementCounter3(){
+        counter3++;
+    }
+
+    public void incrementCounter4(){
+        counter4++;
     }
 }
