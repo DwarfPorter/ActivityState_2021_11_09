3bd579d54967f13f5afbe43168a7e7ae8fe94fe2
DwarfPorter <mrvlmor@gmail.com>
2021-11-09 21:56:01 +0300

    кнопка 3 и 4

--- app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
+++ app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.TextView;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Counters counters;
 
     private TextView textCounter1;
     private TextView textCounter2;
+    private TextView textCounter3;
+    private TextView textCounter4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +29,27 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         textCounter1 = findViewById(R.id.textView1);
         textCounter2 = findViewById(R.id.textView2);
+        textCounter3 = findViewById(R.id.textView3);
+        textCounter4 = findViewById(R.id.textView4);
         initButton2ClickListener();
     }
 
     private void initButton2ClickListener() {
         Button button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counters.incrementCounter2();
-                setTextCounter(textCounter2, counters.getCounter2());
-            }
+        button2.setOnClickListener(view -> {
+            counters.incrementCounter2();
+            setTextCounter(textCounter2, counters.getCounter2());
         });
+    }
+
+    private void initButton3ClickListener(){
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    private void initButton4ClickListener(){
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(button4ClickListener);
     }
 
     public void button1_onClick(View view) {
@@ -49,4 +60,18 @@ public class MainActivity extends AppCompatActivity {
     private void setTextCounter(TextView textCounter1, int counter1) {
         textCounter1.setText(String.format(Locale.getDefault(), "%d", counter1));
     }
+
+    @Override
+    public void onClick(View view) {
+        counters.incrementCounter3();
+        setTextCounter(textCounter3, counters.getCounter3());
+    }
+
+    private final View.OnClickListener button4ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            counters.incrementCounter4();
+            setTextCounter(textCounter4, counters.getCounter4());
+        }
+    };
 }
