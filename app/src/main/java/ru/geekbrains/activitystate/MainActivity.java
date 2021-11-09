789033fa2ddcddd7b44133420d70df1122e1aeac
DwarfPorter <mrvlmor@gmail.com>
2021-11-09 21:23:15 +0300

    Обработка первой кнопки

--- app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
+++ app/src/main/java/ru/geekbrains/activitystate/MainActivity.java
@@ -3,12 +3,26 @@ package ru.geekbrains.activitystate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter1 = 0;
+    private TextView textCounter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCounter1 = findViewById(R.id.textView1);
+    }
+
+    public void button1_onClick(View view) {
+        counter1++;
+        textCounter1.setText(String.format(Locale.getDefault(), "%d", counter1));
     }
 }
